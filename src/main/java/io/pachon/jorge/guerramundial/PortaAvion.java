712c2a22e589a81b6b1d2e048f2c72d6aca3e526
jopachon <jopachon@personalsoft.com.co>
2019-12-30 19:26:06 -0500

    Se lanza ataque de los vehiculos que son Bombarderos.

--- src/main/java/io/pachon/jorge/guerramundial/PortaAvion.java
+++ src/main/java/io/pachon/jorge/guerramundial/PortaAvion.java
@@ -1,7 +1,11 @@
 package io.pachon.jorge.guerramundial;
 
-public class PortaAvion extends Vehiculo {
+public class PortaAvion extends Vehiculo implements ReleasableAtDiscretion{
     public PortaAvion(int capacidadPersonas, int capacidadToneladas, int capacidadMunicion) throws Exception{
         super(capacidadPersonas, capacidadToneladas, capacidadMunicion);
+    }
+
+    public void doRelease() {
+
     }
 }
