84dc7188db9e66ebbe925ca2de742dd4cfe6e503
jopachon <jopachon@personalsoft.com.co>
2019-12-30 19:29:41 -0500

    Se lanza un ataque de los vehiculos Cuerpo A Cuerpo Tierra.

--- src/main/java/io/pachon/jorge/guerramundial/Motocicleta.java
+++ src/main/java/io/pachon/jorge/guerramundial/Motocicleta.java
@@ -1,7 +1,11 @@
 package io.pachon.jorge.guerramundial;
 
-public class Motocicleta extends Vehiculo {
+public class Motocicleta extends Vehiculo implements WeaponableMode {
     public Motocicleta(int capacidadPersonas, int capacidadToneladas, int capacidadMunicion) throws Exception{
         super(capacidadPersonas, capacidadToneladas, capacidadMunicion);
+    }
+
+    public void doWeapon() {
+
     }
 }
